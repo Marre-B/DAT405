5fd290521592517224460622147a2e13bf18602e
Martin Blom <martinbl@net.chalmers.se>
2023-01-31 15:37:46 +0100

    Lab_2 (complete)

--- Lab_2/main.py
+++ Lab_2/main.py
@@ -8,7 +8,7 @@ import sklearn.neighbors as skn
 from sklearn.datasets import load_iris
 from sklearn.linear_model import LogisticRegression
 
-## Problem 1
+## Problem 1 ---------------------------------------------------------------
 
 # Load data
 data = pd.read_csv('data_assignment2.csv')
@@ -31,7 +31,6 @@ print("100m^2 price: " + str(poly1d_fn(100)))
 print("150m^2 price: " + str(poly1d_fn(150)))
 print("200m^2 price: " + str(poly1d_fn(200)))
 
-
 # Plot graph
 plt.plot(area, price, 'yo', area, poly1d_fn(area), '--k')
 plt.xlabel('Living area')
@@ -44,7 +43,8 @@ diff = poly1d_fn(area) - price
 plt.plot(area, diff, 'or')
 plt.show()
 
-## Problem 2
+
+## Problem 2 ---------------------------------------------------------------
 
 def to_target(x):
     return list(dataset.target_names)[x]
@@ -62,15 +62,37 @@ y = df['target_names'].values
 # split taining and test set
 X_train, X_test, y_train, y_test = skm.train_test_split(X, y)
 
-# train the model
-model = LogisticRegression().fit(X_train, y_train)
-y_pred = model.predict(X_test)
+# train the model at logistic regression
+model_lr = LogisticRegression().fit(X_train, y_train)
+y_pred_lr = model_lr.predict(X_test)
+
+# train the model at KNN
+model_knn = skn.KNeighborsClassifier(n_neighbors=100, weights="distance").fit(X_train, y_train)
+y_pred_knn = model_knn.predict(X_test)
 
 # compute accuracy of the model
-print(model.score(X_test, y_test))
+print("LR model accurcy: " + str(model_lr.score(X_test, y_test)))
+print("KNN model accuracy: " + str(model_knn.score(X_test, y_test)))
+
+arr = []
+for x in range(1,100):
+    model_knn = skn.KNeighborsClassifier(n_neighbors=x, weights="distance").fit(X_train, y_train)
+    y_pred_knn = model_knn.predict(X_test)
+    temp = model_knn.score(X_test, y_test)
+    print("KNN model accuracy for " + str(x) + " neighbors: " + str(temp))
+    arr.append(temp)
+
+coef = np.polyfit(range(1, 100), arr, 1)
+poly1d_fn = np.poly1d(coef)
+
+plt.plot(range(1,100), arr, 'yo', arr, poly1d_fn(arr), '--k')
+plt.xlabel('Amount of neighbors')
+plt.ylabel('Accuracy')
+plt.title('Amount of neighbors vs Accuracy')
+plt.show()
 
 # Plot confusion matrix
-confusion_matrix = sk.confusion_matrix(y_test, y_pred)
+confusion_matrix = sk.confusion_matrix(y_test, y_pred_knn)
 cm_display = sk.ConfusionMatrixDisplay(confusion_matrix = confusion_matrix)
 
 cm_display.plot()
